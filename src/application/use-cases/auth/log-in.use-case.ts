58afec37fe36f417020fde86b4a4b63967748551
valhize <190005533+Kritzeey@users.noreply.github.com>
2026-03-01 12:30:04 +0700

    feat: add log in use-case

---
+++ src/application/use-cases/auth/log-in.use-case.ts
@@ -0,0 +1,32 @@
+import type { IUserRepository } from "../../ports/users/user.repository.interface";
+import type { IPasswordHasher } from "../../ports/auth/password-hasher.interface";
+import type { ITokenGenerator } from "../../ports/auth/token-generateor.interface";
+
+export class LogInUseCase {
+  constructor(
+    private userRepository: IUserRepository,
+    private passwordHasher: IPasswordHasher,
+    private tokenGenerator: ITokenGenerator,
+  ) {}
+
+  async execute(username: string, password: string): Promise<string> {
+    const user = await this.userRepository.findByUsername(username);
+
+    if (!user) {
+      throw new Error("Invalid username or password.");
+    }
+
+    const valid = await this.passwordHasher.compare(password, user.password);
+
+    if (!valid) {
+      throw new Error("Invalid username or password.");
+    }
+
+    const token = this.tokenGenerator.generateToken({
+      sub: user.id,
+      user: user.username,
+    });
+
+    return token;
+  }
+}
